ffee2f8298740a25dd3721ed6d83ae768409af6f
arifur946954 <arif.bd08060@gmail.com>
2023-10-29 22:40:43 +0600

    advance mapping

---
+++ JpaCrud/src/main/java/com/jpa/JpaCrud/JpaCrudApplication.java
@@ -0,0 +1,14 @@
+package com.jpa.JpaCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaCrudApplication {
+	//this is test
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaCrudApplication.class, args);
+	}
+
+}
---
+++ JpaCrud/src/test/java/com/jpa/JpaCrud/JpaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.JpaCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
