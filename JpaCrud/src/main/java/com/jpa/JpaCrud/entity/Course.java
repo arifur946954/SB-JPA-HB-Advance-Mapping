8ec8b71637fedd4ac9287f22be93a4c06458d26f
arifur946954 <arif.bd08060@gmail.com>
2023-11-05 00:03:27 +0600

    Courses

--- JpaCrud/src/main/java/com/jpa/JpaCrud/entity/Course.java
+++ JpaCrud/src/main/java/com/jpa/JpaCrud/entity/Course.java
@@ -7,4 +7,48 @@ import jakarta.persistence.Table;
 @Table(name = "course")
 
 public class Course {
+    private int id;
+    private String title;
+    private  String instructor;
+
+    public Course() {
+    }
+
+    public Course(String title, String instructor) {
+        this.title = title;
+        this.instructor = instructor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", instructor='" + instructor + '\'' +
+                '}';
+    }
 }
